f2d9a523f6bab95b48c7f227bbd0c9f07705f0b1
Mayank Agrawal <mayank.agrawalbca@gmail.com>
2023-05-30 22:31:23 +0530

    Practice_set_1

---
+++ script.js
@@ -0,0 +1,44 @@
+// Quiz 1. Write a js program to store name , phone no. , marks of a student using object.
+// let ram={
+//     phone : '9058225555',
+//     marks : "Hindi : 10 , English : 15 , Maths :20"
+// }
+// console.log(ram['marks'])
+
+// Practice Set 1
+
+// Que 1. create a VARIABLE OF TYPE STRING and try to add number on it.
+// let a = "hello";
+// let b = 10;
+// console.log(a+b)
+
+// Que 2. Use typeof operator to find a type of variable of a last question
+// console.log(typeof(a+b))
+
+// Que 3. Create a Const object in js Can u change it to hold a number later?
+// const q = {
+//     name : "Mayank"
+// }
+// q = 10
+// Ans. No,It give an error because const does not redeclare in js.
+
+// Que 4. Try to add a new key to the const object in problem 3 where u do it.
+// q["class"] = 12
+// q["Section"] = "B"
+// console.log(q)
+
+// Que 5. Write a js program to create a word meaning dictionary of 5 words.
+// const dict = {
+//     Athleisure : "Comfortable and casual footwear & clothing designed for exercise and rigorous activity.",
+//     Broigus : "Angry or irritated.",
+//     By_Catch : "A catch of fish that cannot be put to commercial use.",
+//     Blert : "A cowardly person, someone who is weak.",
+//     Comp : "Providing products or services free of charge as a token of appreciation, a favor.",
+// }
+// console.log(dict)
+// console.log(dict.Athleisure)
+// console.log(dict.Broigus)
+// console.log(dict.By_Catch)
+// console.log(dict.Blert)
+// console.log(dict.Comp)
+console.log(complete)
